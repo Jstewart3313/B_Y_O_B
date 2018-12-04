7060fae80270e05f1fcc07975e6c0c160a763c1a
Alex Bruce <brucealmightyalex@yahoo.com>
2018-12-04 15:35:14 -0700

    Add all necessary empty endpoints

--- server.js
+++ server.js
@@ -8,6 +8,43 @@ app.get('/', (request, response) => {
   response.send('BYOB');
 });
 
+app.get('/', (request, response) => {
+  // DO THINGS HERE
+});
+
+app.get('/', (request, response) => {
+  // DO THINGS HERE
+});
+
+app.get('/', (request, response) => {
+  // DO THINGS HERE
+});
+
+app.post('/', (request, response) => {
+  // DO THINGS HERE
+});
+
+app.post('/', (request, response) => {
+  // DO THINGS HERE
+});
+
+app.put('/', (request, response) => {
+  // DO THINGS HERE
+});
+
+app.patch('/', (request, response) => {
+  // DO THINGS HERE
+});
+
+app.delete('/', (request, response) => {
+  // DO THINGS HERE
+});
+
+app.delete('/', (request, response) => {
+  // DO THINGS HERE
+});
+
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
